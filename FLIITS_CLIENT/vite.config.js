fa9f08399661e5371169a8132aaf16a7ebe7dba3
unknown <yokasdenis@gmail.com>
2025-04-01 13:07:44 +0300

    updated vite in json package

--- FLIITS_CLIENT/vite.config.js
+++ FLIITS_CLIENT/vite.config.js
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs(),
+    nodeResolve()
+  ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
   server: {
     proxy: {
       '/api': {
