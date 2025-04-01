96cb8ede17e3fa3abfd36175dd049943892d03df
unknown <yokasdenis@gmail.com>
2025-04-01 13:01:37 +0300

    updated vite in json package

--- FLIITS_CLIENT/vite.config.js
+++ FLIITS_CLIENT/vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    commonjs(),
-    nodeResolve()
-  ],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
-  },
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
